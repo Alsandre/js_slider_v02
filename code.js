7ca65c3675c2b37e68d5685696500ac3e8affef4
Aleksandre <gabeditebuli@gmail.com>
2023-02-22 21:30:45 +0400

    import check

--- code.js
+++ code.js
@@ -1,28 +1,5 @@
-const source = [];
-for (let i = -1; i < 23; i++) {
-  if (i === -1 || i === 0) {
-    let imgString = `<img src ='./assets/silhouette-left.png' class='img-styles' >`;
-    let imgElement = document.createElement("img");
-    imgElement.src = `./assets/silhouette-left.png`;
-    imgElement.classList.add("img-styles");
-    let item = { imgElement, imgString };
-    source.push(item);
-  } else if (i === 21 || i === 22) {
-    let imgString = `<img src ='./assets/silhouette-right.png' class='img-styles' >`;
-    let imgElement = document.createElement("img");
-    imgElement.src = `./assets/silhouette-right.png`;
-    imgElement.classList.add("img-styles");
-    let item = { imgElement, imgString };
-    source.push(item);
-  } else {
-    let imgString = `<img src ='./assets/cat${i}.png' class='img-styles' >`;
-    let imgElement = document.createElement("img");
-    imgElement.src = `./assets/cat${i}.png`;
-    imgElement.classList.add("img-styles");
-    let item = { imgElement, imgString };
-    source.push(item);
-  }
-}
+import {source} from './constants.js';
+
 const cat_walk = () => {
   let catWalkLeft = document.getElementById("cat-walk-left");
   let catWalkRight = document.getElementById("cat-walk-right");
